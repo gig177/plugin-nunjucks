53b2d9012a3afab7395adbf5e02ae6c90092faf1
Evgeniy Poturaev <e.poturaev@connect2me.ru>
2015-10-01 17:42:25 +0300

    jspm config

---
+++ view/config.js
@@ -0,0 +1,53 @@
+System.config({
+  baseURL: "/view/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  packages: {
+    "app": {
+      "main": "main",
+      "meta": {
+        "*.html": {
+          "loader": "nj"
+        }
+      }
+    }
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.2.0",
+    "jquery": "github:components/jquery@2.1.4",
+    "nunjucks": "github:mozilla/nunjucks@2.1.0",
+    "path": "github:jspm/nodelibs-path@0.1.0",
+    "text": "github:systemjs/plugin-text@0.0.2",
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.25": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.2.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
