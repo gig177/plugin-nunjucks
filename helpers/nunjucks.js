581d089fe9337221d891e4d8fcd740d6d7ed7c1f
Evgeniy Poturaev <gig177@yandex.ru>
2015-11-04 16:49:19 +0300

    using origin snippet

--- helpers/nunjucks.js
+++ helpers/nunjucks.js
@@ -1,30 +1,7 @@
 import nj from 'nunjucks';
+import PrecompiledLoader from 'nunjucks/src/precompiled-loader';
 
-export var PrecompiledLoader = nj.Loader.extend({
-    init: function(compiledTemplates) {
-        // setup a process which watches templates here
-        // and call `this.emit('update', name)` when a template
-        // is changed
-        this.precompiled = compiledTemplates || window.nunjucksPrecompiled || {};
-    },
-
-    getSource: function(name) {
-        // load the template
-        // return an object with:
-        //   - src:     String. The template source.
-        //   - path:    String. Path to template.
-        //   - noCache: Bool. Don't cache the template (optional).
-        return {
-            src: {
-                type: 'code',
-                obj: this.precompiled[name],
-            },
-            path: name,
-        };
-    },
-});
-
-export var env = new nj.Environment( new PrecompiledLoader() ); 
+export var env = new nj.Environment( new PrecompiledLoader(window.nunjucksPrecompiled) ); 
 export default function(tpl, ctx) {
     return env.render(tpl, ctx);
 }
