6b9ccf0fd2d5a6352ede4c9176fdb11ba8f7b0cd
Evgeniy Poturaev <gig177@ya.ru>
2015-11-04 10:44:00 +0300

    using express instead flask

--- run.py
+++
@@ -1,6 +0,0 @@
-#!/bin/env python
-from app import app
-
-host = '127.0.0.1'
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, host=host)
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    nj = require('nunjucks');
+var app = express();
+
+app.get('/', function(req, res) {
+    var dev = req.query.dev;
+    if (!dev) dev = 1;
+    var ctx = { DEV : dev, tpl : req.query.page };
+    console.log(ctx);
+    res.send( nj.render('tpl/index.html', ctx) );
+});
+app.use( express.static('.') );
+
+app.listen(3000);
