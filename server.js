8fdd30309a3ddbc79ab191a488ae59bb07e6f43a
Evgeniy Poturaev <gig177@ya.ru>
2015-11-14 21:01:12 +0300

    passing the njOptions at server runtime

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var app = express();
 app.get('/', function(req, res) {
     var dev = req.query.dev;
     if (!dev) dev = 1;
-    var ctx = { DEV : dev, tpl : req.query.tpl };
+    var ctx = {
+        DEV: dev,
+        tpl: req.query.tpl,
+        lang: req.query.lang
+    };
     console.log(ctx);
     res.send( nj.render('tpl/index.html', ctx) );
 });
